4b2d62639518922eb0b280ea06ce8114936c4e7c
SaulSagm <47869033+SaulSagm@users.noreply.github.com>
2019-02-22 18:17:12 -0600

    Create lab.c

---
+++ lab.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+int main()
+{
+  int height;
+  scanf("%i",&height);
+  
+  char initial[height][3];
+  
+  int=a;
+  int=b;
+  int=c;
+  float time=0.5*a+1*b+0.5*c;
+  
+  
+
+}
