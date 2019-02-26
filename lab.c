a2bbc48467ad4e8e6d2038e800b54c64cf521c41
SaulSagm <47869033+SaulSagm@users.noreply.github.com>
2019-02-26 17:04:33 -0600

    Update lab.c

--- lab.c
+++ lab.c
@@ -4,8 +4,8 @@
 
 int main()
 {
-    int i,height,a=0,b=0,c=0,originb,originc;
-    char initial[100],goal[100],letters[4]={'A','B','C','D'};
+    int i,height,a=0,b=0,c=0,originb=0,originc=0;
+    char initial[50],goal[50],letters[4]={'A','B','C','D'};
     
     scanf("%i",&height);
     scanf("%s",&initial);
@@ -35,20 +35,36 @@ int main()
           c++;
         }
         else
-          if(letters[0]=initial[i]||letters[1]=initial[i]||letters[2]=initial[i]||letters[3]=initial[i])
+          if(letters[0]==initial[i]||letters[1]==initial[i]||letters[2]==initial[i]||letters[3]==initial[i])
           {
             state[b][c]=initial[i];
           }
+          else
+            if((initial[i]=='('&&initial[i+1]==')')||initial[i]=='X')
+            {
+                state[b][c]=' ';
+            }
     }
     
-    /*for(b=0;b<height;b++)
+    for(b=0;b<height;b++)
+    {
+        for(c=0;c<=a;c++)
+        {
+            if(letters[0]!=state[b][c]&&letters[1]!=state[b][c]&&letters[2]!=state[b][c]&&letters[3]!=state[b][c]);
+            {
+                state[b][c]='-';
+            }
+        }
+    }
+    
+    for(b=0;b<height;b++)
     {
         for(c=0;c<=a;c++)
         {
             printf("%c \t",state[b][c]);
         }
         printf("\n");
-    }*/
+    }
 
     char end[height][a+1];
     b=height-1;
@@ -67,29 +83,46 @@ int main()
           c++;
         }
         else
-          if(letters[0]=goal[i]||letters[1]=goal[i]||letters[2]=goal[i]||letters[3]=goal[i])
+          if(letters[0]==goal[i]||letters[1]==goal[i]||letters[2]==goal[i]||letters[3]==goal[i])
           {
             end[b][c]=goal[i];
           }
+          else
+              if(goal[i]=='('&&goal[i+1]==')'||goal[i]=='X')
+              {
+                end[b][c]=' ';
+              }
     }
     
-    /*for(b=0;b<height;b++)
+    for(b=0;b<height;b++)
+    {
+        for(c=0;c<=a;c++)
+        {
+            if(letters[0]!=end[b][c]&&letters[1]!=end[b][c]&&letters[2]!=end[b][c]&&letters[3]!=end[b][c]);
+            {
+                end[b][c]='-';
+            }
+        }
+    }
+    
+    for(b=0;b<height;b++)
     {
         for(c=0;c<=a;c++)
         {
             printf("%c \t",end[b][c]);
         }
         printf("\n");
-    }*/
+    }
     
-    for(b=0;b<height;b++)
+    for(c=0;c<=a;c++)
     {
-        for(c=0;c<=a;c++)
+        for(b=0;b<height;b++)
         {
             if(state[b][c]!=end[b][c])
             {
               originb=b;
               originc=c;
+              printf("%i %i \n",originb,originc);
               b=height;
               c=a+1;
             }
@@ -100,9 +133,9 @@ int main()
     {
         for(c=0;c<=a;c++)
         {
-            if(state[originb][originc]==end[b][c]&&state[b][c]==' '&&state[b][c]=='\0')
+            if(state[originb][originc]==end[b][c]&&state[b][c]==' ')
             {
-              printf("(%i,%i)",originc,c);
+              //printf("%i %i",originc,c);
             }
         }
     }
