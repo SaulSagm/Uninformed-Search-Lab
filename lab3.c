8803a59e027847a14547467d62a61b198ce2d1dc
SaulSagm <47869033+SaulSagm@users.noreply.github.com>
2019-02-28 23:49:15 -0600

    Update lab3.c

--- lab3.c
+++ lab3.c
@@ -5,21 +5,23 @@
 int main()
 {
     int i,j;
-    int a=0,b=0,c=0;
+    int a=0,b=0;
+    int c=0,d=0;
     int height,width;
-    int row=0,column=0;
-    char initial[10],goal[10];
+    int row1=0,column1=0;
+    int row2=0,column2=0;
+    char initial[20],goal[20];
     
     scanf("%i",&height);
     scanf("%s",&initial);
     scanf("%s",&goal);
 
-    b=strln(initial);
-    c=strln(goal);
+    b=strlen(initial);
+    c=strlen(goal);
 
-    for(i=0;i<strlen(initial);i++)
+    for(i=0;i<b;i++)
     {
-        if(initial[i]==';')
+        if(initial[i]==59)
         {
             a++;
         }
@@ -27,62 +29,96 @@ int main()
     width=a+1;
     
     char *state[height][width];
-    row=height-1;
+    char *end[height][width];
+    row1=height-1;
+    row2=height-1;
+    
+    for(i=0;i<height;i++)
+    {
+        for(j=0;j<width;j++)
+        {
+            state[i][j]=45;
+        }
+    }
     
     for(i=0;i<b;i++)
     {
-        switch(initial[i])
-        {
-            case(44):
-                row--;
-            break;
-            case(59):
-                row=height-1;
-                column++;
-            break;
-            case(65):
-                state[row][column]=65;
-            break;
-            case(66):
-                state[row][column]=66;
-            break;
-            case(67):
-                state[row][column]=67;
-            break;
-            case(68):
-                state[row][column]=68;
-            break;
-            case(40):
-                if(initial[i+1]==41)
-                {
-                    state[row][column]=32;
-                }
-            break;
-            case(88):
-                state[row][column]=32;
-            break;
-        }
-    }
-    
-    for(b=0;b<height;b++)
-    {
-        for(c=0;c<=a;c++)
-        {
-            if(state[b][c]!='A'&&state[b][c]!='B'&&state[b][c]!='C'&&state[b][c]!='D');
+        if(initial[i]==44)
+            row1--;
+        else
+            if(initial[i]==59)
             {
-                state[b][c]='-';
+                row1=height-1;
+                column1++;
             }
-        }
+            else
+                if(initial[i]==65)
+                    state[row1][column1]='A';
+                else
+                    if(initial[i]==66)
+                        state[row1][column1]='B';
+                    else
+                        if(initial[i]==67)
+                            state[row1][column1]='C';
+                        else
+                            if(initial[i]==68)
+                                state[row1][column1]='D';
     }
     
-    for(b=0;b<height;b++)
+    for(i=0;i<height;i++)
     {
-        for(c=0;c<=a;c++)
+        for(j=0;j<width;j++)
         {
-            printf("%c \t",state[b][c]);
+            printf("%c \t",state[i][j]);
         }
         printf("\n");
     }
-
+    
+    for(i=0;i<height;i++)
+    {
+        for(j=0;j<width;j++)
+        {
+            end[i][j]=45;
+        }
+    }
+    
+    for(i=0;i<c;i++)
+    {
+        if(goal[i]==44)
+            row2--;
+        else
+            if(goal[i]==59)
+            {
+                row2=height-1;
+                column2++;
+            }
+            else
+                if(goal[i]==65)
+                    end[row2][column2]='A';
+                else
+                    if(goal[i]==66)
+                        end[row2][column2]='B';
+                    else
+                        if(goal[i]==67)
+                            end[row2][column2]='C';
+                        else
+                            if(goal[i]==68)
+                                end[row2][column2]='D';
+    }
+    
+    for(i=0;i<height;i++)
+    {
+        for(j=0;j<width;j++)
+        {
+            printf("%c \t",end[i][j]);
+        }
+        printf("\n");
+    }
+    
+    if(state[0][1]==end[0][1])
+        printf("Iguales");
+    else
+        printf("Diferentes");
+    
     return(0);
 }
